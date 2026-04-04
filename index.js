007fea2d25665c73a1b7cb0115e1a932c062ad5a
Srasti Jain <srasti965@gmail.com>
2026-04-04 16:14:43 +0530

    Add GET /posts endpoint and new styling

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ app.use(express.urlencoded({extended:true}));
 const path = require("path");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"public")));
-app.get("/",(req,res)=>{
-    res.send("serving working well!");
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts= [
+    {Username: "Srasti",Content:"I don't know how, but i will"},
+    {Username: "Srast2j84",Content:"Consistency>Motivation"},
+    {Username: "TerminalThinker",Content:"No filter, no facade,just truth"},
+    {Username: "Miss Jain",Content:"Full-stack web developer sharpening my skills and actively practicing DSA."},
+    {Username: "Nova",Content:"Looking for internship opportunities. Open to new experiences and eager to contribute."},
+]
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
 });
 
 app.listen(port,()=>{
